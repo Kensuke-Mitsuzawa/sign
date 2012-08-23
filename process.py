9b6fb467a44726fd856149e626579502aabc32cd
Kensuke Mitsuzawa <kensuke-mi@is.naist.jp>
2012-08-23 19:31:50 +0900

    少しだけ進歩した

--- process.py
+++ process.py
@@ -6,6 +6,7 @@ import sys
 import codecs
 import re
 
+# 結局、使ってないけど一応残しておく
 class word_info:
 
     def __init__(self,word,pos,pos_m,func,func_m,kana):
@@ -35,7 +36,7 @@ cf = cabocha_file.readlines()
 cf.pop()
 
 for line in cf:
-    print line
+    #print line
 
     if len(line) == 22:
         #char_listをリセットする
@@ -58,14 +59,15 @@ for line in cf:
             tag_info_list.append(u"-")
             tag_info_list.append(int(char_list[1]))
             tag_info_list.append(u"D")
-
+            #処理にまったく関係ないが見やすくするために|をいれる。
+            tag_info_list.append(u"|")
             continue
                 
         if re.compile("[1-9]").search(char_list[0]):
             tag_info_list.append(u"*")
             tag_info_list.append(int(char_list[0]))
             tag_info_list.append(u"D")
-
+            tag_info_list.append(u"|")
             continue
             
             #これで末尾に*,番号,D]か-,番号,D]のどちらかのリストができあがっているはず
@@ -73,43 +75,52 @@ for line in cf:
 
     if len(line) > 23:
 
-            # ","で区切ってリストに追加する
+        # ","で区切ってリストに追加する
+        word_info_list = line.split(",")
 
-            word_info_list = line.split(",")
-        
+        #語情報の先頭は空白でつながったままなので、分解して「語」をtag_info_listの最後に、「品詞」をword_info_listの先頭に入れる。
+        #だいぶ後だしじゃんけん的な処理なので、コードを書き換える時は要注意
+        tmp = word_info_list[0]
+        tmp_splitted = tmp.split(" ")
+        tag_info_list.append(tmp_splitted[0])
+        word_info_list[0] = tmp_splitted[-1]
 
-            #リスト末尾にブロック内番号を追加
-            word_info_list.append(no_in_block)
+        #リスト末尾にブロック内番号を追加
+        word_info_list.append(no_in_block)
             
 
-            #ホントはもっと後でする処理だけど、書き忘れ防止に。
-            no_in_block += 1
+        #ホントはもっと後でする処理だけど、書き忘れ防止に。
+        no_in_block += 1
             
-            #２つの情報リストを結合する
-            info_list = tag_info_list + word_info_list
+        #２つの情報リストを結合する
+        info_list = tag_info_list + word_info_list
 
-            key = str(tag_info_list[1]) + str(no_in_block)
-            # ここでエラーが出るようならぜんぶstrにしてしまっていいだろう
+        key = str(tag_info_list[1]) + str(no_in_block)
+        # ここでエラーが出るようならぜんぶstrにしてしまっていいだろう
 
-            d = {}
+        d = {}
 
-            for tmp in info_list:
-                d.setdefault(key,[]).append(tmp)
+        for tmp in info_list:
+            d.setdefault(key,[]).append(tmp)
 
-            print d
+        #print d
 
     else :
         pass
 
 
+#ここより以下は関数にしてもいいんだろうが、めんどくさいのでパス
 
+
+#リストの復元    
+dic_list = d.values()
+info_list_ = dic_list[0]
+
+#ここからルールを書いていく。
 
 
     
-
-#for line in cf:
     
-#input_sentence = sys.stdin.readline()
-#input_sentence = input_sentence.decode('utf-8')
+    
 
 
