1cf008f9e33d28b0ccd3e5d2711932af9cb16f18
kensuke-mi <kensuke-mi@is.naist.jp>
2013-01-18 00:04:05 +0900

    delete def negative, and made module negative.py

---
+++ negative.py
@@ -0,0 +1,98 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+
+def find_negation(clause_list,clause_num,clause):
+    #否定形 or not 否定じゃなければ１を返す
+    negative_value = 0
+    
+    #カウンターの設置
+    counter = 0
+    
+    #ここで新しい節を読みおなし
+    for value in clause:
+
+        if counter == 0:
+            start_pos = 1
+            end_pos = value
+
+        else:
+            start_pos = end_pos + 1
+            end_pos = end_pos + value
+
+        counter += 1
+
+        #情報の抽出を正規表現でしていく
+        for i in range(start_pos,end_pos + 1):
+            sentence = clause_list[i]
+            
+            #+以降が単語に関する情報の（はず）なので、ここから情報を抽出する
+            if not re.findall(r"\+.*D",sentence) == []:
+                if not re.findall(r"<否定表現>",sentence) == []:
+                    #否定なら１を返す
+                    negative_value = 1
+
+
+    return negative_value
+
+
+
+def negation(clause_list,clause_num,clause):
+    #否定文に関する処理を行う
+    print "--------------------------"
+    print "About negative information\n"
+
+    #どの否定形か？を管理する変数
+    negative = ""
+    
+    #カウンターの設置
+    counter = 0
+    
+    #ここで新しい節を読みおなし
+    for value in clause:
+
+        if counter == 0:
+            start_pos = 1
+            end_pos = value
+
+        else:
+            start_pos = end_pos + 1
+            end_pos = end_pos + value
+
+        counter += 1
+
+        #情報の抽出を正規表現でしていく
+        for i in range(start_pos,end_pos + 1):
+            sentence = clause_list[i]
+            
+            #+以降が単語に関する情報の（はず）なので、ここから情報を抽出する
+            if not re.findall(r"\+.*D",sentence) == []:
+                if not re.findall(r"<否定表現>",sentence) == []:
+                    choice = raw_input("1 意思の否定 2 所有・存在の否定 3 完了の否定 4 不可能の意味 5 経験の否定 6 必要性の否定\n")
+                    choice = int(choice)
+                    if choice == 1:
+                        negative = "willness"
+                    if choice == 2:
+                        negative = "posess"
+                    if choice == 3:
+                        negative = "perfect"
+                    if choice == 4:
+                        negative = "impossible"
+                    if choice == 5:
+                        negative = "experience"
+                    if choice == 6:
+                        negative = "need"
+
+            #否定表現がなかった時。つまり否定文じゃなかった時
+            #else:
+            #    negative = ""
+
+    #-------------------------------
+    #単なる表示の都合上
+    if not negative == "":
+        print negative,"is selected"
+    #-------------------------------
+                   
+    return negative
+
--- sign_knp.py
+++ sign_knp.py
@@ -3,7 +3,7 @@
 
 
 import sys,codecs,subprocess,readline,re
-
+import negative
 from types import *
 
 ## 辞書の定義
@@ -22,67 +22,6 @@ class info:
         self.case = case
         self.case_analiyzed = case_ana
         self.order = order
-
-def negative(clause_list,clause_num,clause):
-    #否定文に関する処理を行う
-    print "--------------------------"
-    print "About negative information\n"
-
-    #どの否定形か？を管理する変数
-    negative = ""
-    
-    #カウンターの設置
-    counter = 0
-    
-    #ここで新しい節を読みおなし
-    for value in clause:
-
-        if counter == 0:
-            start_pos = 1
-            end_pos = value
-
-        else:
-            start_pos = end_pos + 1
-            end_pos = end_pos + value
-
-        counter += 1
-
-        #情報の抽出を正規表現でしていく
-        for i in range(start_pos,end_pos + 1):
-            sentence = clause_list[i]
-            
-            #+以降が単語に関する情報の（はず）なので、ここから情報を抽出する
-            if not re.findall(r"\+.*D",sentence) == []:
-                if not re.findall(r"<否定表現>",sentence) == []:
-                    #print "否定表現が見つかりました。どうするか考えてください。半角英数で打ち込んでネ☆\n１たたかう２にげる３ねる\n"
-                    choice = raw_input("1 意思の否定 2 所有・存在の否定 3 完了の否定 4 不可能の意味 5 経験の否定 6 必要性の否定\n")
-                    choice = int(choice)
-                    #眠いのでアレだが、いちいち文字で否定種類をわけるのはいかがなものかと。数字でわけてもいいんじゃね。
-                    if choice == 1:
-                        negative = "willness"
-                    if choice == 2:
-                        negative = "posess"
-                    if choice == 3:
-                        negative = "perfect"
-                    if choice == 4:
-                        negative = "impossible"
-                    if choice == 5:
-                        negative = "experience"
-                    if choice == 6:
-                        negative = "need"
-
-            #否定表現がなかった時。つまり否定文じゃなかった時
-            #else:
-            #    negative = ""
-
-    #-------------------------------
-    #単なる表示の都合上
-    if not negative == "":
-        print negative,"is selected"
-    #-------------------------------
-                        
-    return negative
-
 
 def Syori(clause_list,clause_num,clause,negative_choice):
     #clause_listは節ごとに切った解析結果、clause_numは節の数、clauseは各節が何行分の情報を持っているか？リスト
@@ -727,7 +666,14 @@ def knp_tab(sentence):
         
     #ここで各処理関数に情報を投げる
     clause_num, clause = clause_count(tmp_list)
-    negative_choice = negative(clause_list,clause_num,clause)
+    #returns 0 if not negation, returns 1 if negation
+    negative_value = negative.find_negation(clause_list,clause_num,clause)
+    
+    if negative_value == 0:
+        negative_choice = ""
+    else:
+        negative_choice = negative.negation(clause_list,clause_num,clause)
+
     info_dic,struc_dic = Syori(clause_list,clause_num,clause,negative_choice)
     set_dic = make_case_set(info_dic)
     if not negative_choice == "":
